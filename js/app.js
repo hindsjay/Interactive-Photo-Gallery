8ac7776a9903693ecd4fbfc56559a0f9aafae769
Jay Hinds <hinds.jason@gmail.com>
2019-08-04 17:18:51 -0400

    add code for search bar functionality

--- js/app.js
+++ js/app.js
@@ -6,10 +6,27 @@ $(document).ready(function() {
     showThumbByDefault: false,
     mode: 'lg-fade',
     share: false,
-    zoom: false,
     fullScreen: false,
     autoplayControls: false,
     download: false,
     toggleThumb: false
   }); 
+});
+
+
+// search bar function
+const inputElement = document.querySelector('input');
+const captionNodeList = document.querySelectorAll('div[data-sub-html]');
+
+inputElement.addEventListener('keyup', function(event) {
+  const inputElementValue = event.target.value.toLowerCase();
+  
+  captionNodeList.forEach(function(element) {
+    const captionText = element.getAttribute('data-sub-html');
+    if (captionText.toLowerCase().indexOf(inputElementValue) > -1 ) {
+      element.style.display = '';
+    } else {
+      element.style.display = 'none';
+    }
+  });
 });
